86fbdeec62c558f62836869bd7b34b7a2e4d7364
Lukas Pirl <lukas.pirl@student.hpi.uni-potsdam.de>
2012-03-21 15:10:05 +0100

    Fixes failure of get_latest_revisions() with   cannot concatenate 'str' and 'type(comparator)' objects Fixes https://github.com/stdbrouw/django-revisions/issues/11

--- revisions/models.py
+++ revisions/models.py
@@ -11,12 +11,12 @@ from django.contrib.contenttypes.models import ContentType
 from revisions import managers, utils
 import inspect
 
-# the crux of all errors seems to be that, with VersionedBaseModel, 
+# the crux of all errors seems to be that, with VersionedBaseModel,
 # doing setattr(self, self.pk_name, None) does _not_ lead to creating
 # a new object, and thus versioning as a whole doesn't work
 
 # the only thing lacking from the VersionedModelBase is a version id.
-# You may use VersionedModelBase if you need to specify your own 
+# You may use VersionedModelBase if you need to specify your own
 # AutoField (e.g. using UUIDs) or if you're trying to adapt an existing
 # model to ``django-revisions`` and have an AutoField not named
 # ``vid``.
@@ -27,7 +27,7 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
         base = cls
         while isinstance(base._meta.pk, models.OneToOneField):
             base = base._meta.pk.rel.to
-        return base    
+        return base
 
     @property
     def base_model(self):
@@ -64,7 +64,7 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
         base = self
         while isinstance(base._meta.pk, models.OneToOneField):
             base = base._meta.pk.rel.to
-        return base    
+        return base
 
     @property
     def _base_table(self):
@@ -72,7 +72,7 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
 
     # content bundle id
     cid = models.CharField(max_length=36, editable=False, null=True, db_index=True)
-    
+
     # managers
     latest = managers.LatestManager()
     objects = models.Manager()
@@ -80,7 +80,7 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
     # all related revisions, plus easy shortcuts to the previous and next revision
     def get_revisions(self):
         qs = self.__class__.objects.filter(cid=self.cid).order_by(self.comparator_name)
-        
+
         try:
             qs.prev = qs.filter(**{self.comparator_name + '__lt': self.comparator}).order_by('-' + self.comparator_name)[0]
         except IndexError:
@@ -89,12 +89,12 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
             qs.next = qs.filter(**{self.comparator_name + '__gt': self.comparator})[0]
         except IndexError:
             qs.next = None
-        
+
         return qs
-    
+
     def check_if_latest_revision(self):
         return self.comparator >= max([version.comparator for version in self.get_revisions()])
-    
+
     @classmethod
     def fetch(cls, criterion):
         if isinstance(criterion, int) or isinstance(criterion, str):
@@ -107,24 +107,24 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
                 return cls.objects.filter(**{pub_date + '__lte': criterion}).order('-' + self.comparator_name)[0]
             else:
                 raise ImproperlyConfigured("""Please specify which field counts as the publication
-                    date for this model. You can do so inside a Versioning class. Read the docs 
+                    date for this model. You can do so inside a Versioning class. Read the docs
                     for more info.""")
         else:
             raise TypeError("Can only fetch an object using a primary key, a date or a datetime object.")
 
     def revert_to(self, criterion):
         revert_to_obj = self.__class__.fetch(criterion)
-    
+
         # You can only revert a model instance back to a previous instance.
         # Not any ol' object will do, and we check for that.
         if revert_to_obj.pk not in self.get_revisions().values_list('pk', flat=True):
             raise IndexError("Cannot revert to a primary key that is not part of the content bundle.")
         else:
             return revert_to_obj.revise()
-            
+
     def get_latest_revision(self):
-        return self.get_revisions().order_by('-' + self.comparator)[0]
-    
+        return self.get_revisions().order_by('-' + self.comparator_name)[0]
+
     def make_current_revision(self):
         if not self.check_if_latest_revision():
             self.save()
@@ -140,9 +140,9 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
         def parse_shortcut(unique_together):
             unique_together = tuple(unique_together)
             if len(unique_together) and isinstance(unique_together[0], basestring):
-                unique_together = (unique_together, )    
+                unique_together = (unique_together, )
             return unique_together
-        
+
         # Django actually checks uniqueness for a single field in the very same way it
         # does things for unique_together, something we happily take advantage of
         unique = tuple([(field,) for field in getattr(self.Versioning, 'unique', ())])
@@ -150,10 +150,10 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
             unique + \
             parse_shortcut(getattr(self.Versioning, 'unique_together', ())) + \
             parse_shortcut(getattr(self._meta, 'unique_together', ()))
-        
+
         model = self.__class__()
         model._meta.unique_together = unique_together
-        return models.Model._get_unique_checks(model, exclude)          
+        return models.Model._get_unique_checks(model, exclude)
 
     def _get_attribute_history(self, name):
         if self.__dict__.get(name, False):
@@ -165,12 +165,12 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
         """ This method extends a regular related-manager by also including objects
         that are related to other versions of the same content, instead of just to
         this one object. """
-        
+
         related_model = relatedmanager.model
         related_model_name = related_model._meta.module_name
-        
+
         # The foreign key field name on related objects often, by convention,
-        # coincides with the name of the class it relates to, but not always, 
+        # coincides with the name of the class it relates to, but not always,
         # e.g. you could do something like
         #   class Book(models.Model):
         #       thingmabob = models.ForeignKey(Author)
@@ -181,21 +181,21 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
         #
         # By all means, I'd welcome suggestions for prettier code.
         ref_name = self._meta._name_map[related_model_name][0].field.name
-        pks = [story.pk for story in self.get_revisions()]        
+        pks = [story.pk for story in self.get_revisions()]
         objs = related_model._default_manager.filter(**{ref_name + '__in': pks})
-        
+
         return objs
-    
+
     def __getattr__(self, name):
         # we catch all lookups that start with 'related_'
         if name.startswith('related_'):
             related_name = "_".join(name.split("_")[1:])
             attribute = getattr(self, related_name, False)
             # we piggyback off of an existing relationship,
-            # so the attribute has to exist and it has to be a 
+            # so the attribute has to exist and it has to be a
             # RelatedManager or ManyRelatedManager
             if attribute:
-                # (we check the module instead of using isinstance, since 
+                # (we check the module instead of using isinstance, since
                 # ManyRelatedManager is created using a factory so doesn't
                 # actually exist inside of the module)
                 if attribute.__class__.__dict__['__module__'] == 'django.db.models.fields.related':
@@ -206,7 +206,7 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
             return self._get_attribute_history(attribute)
 
         raise AttributeError(name)
-            
+
     def prepare_for_writing(self):
         """
         This method allows you to clear out certain fields in the model that are
@@ -216,7 +216,7 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
             super(VersionedModelBase, self).__setattr__(field, '')
 
     def validate_bundle(self):
-        # uniqueness constraints per bundle can't be checked at the database level, 
+        # uniqueness constraints per bundle can't be checked at the database level,
         # which means we'll have to do so in the save method
         if getattr(self.Versioning, 'unique_together', None) or getattr(self.Versioning, 'unique', None):
             # replace ValidationError with IntegrityError because this is what users will expect
@@ -229,11 +229,11 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
         self.validate_bundle()
         return self.clone()
 
-    def save(self, *vargs, **kwargs):       
-        # The first revision of a piece of content won't have a bundle id yet, 
-        # and because the object isn't persisted in the database, there's no 
+    def save(self, *vargs, **kwargs):
+        # The first revision of a piece of content won't have a bundle id yet,
+        # and because the object isn't persisted in the database, there's no
         # primary key either, so we use a UUID as the bundle ID.
-        # 
+        #
         # (Note for smart alecks: Django chokes on using super/save() more than
         # once in the save method, so doing a preliminary save to get the PK
         # and using that value for a bundle ID is rather hard.)
@@ -242,17 +242,17 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
 
         self.validate_bundle()
         super(VersionedModelBase, self).save(*vargs, **kwargs)
-        
+
     def delete_revision(self, *vargs, **kwargs):
         super(VersionedModelBase, self).delete(*vargs, **kwargs)
-    
+
     def delete(self, *vargs, **kwargs):
         for revision in self.get_revisions():
             revision.delete_revision(*vargs, **kwargs)
 
     class Meta:
         abstract = True
-    
+
     class Versioning:
         clear_each_revision = []
         publication_date = None
@@ -260,7 +260,7 @@ class VersionedModelBase(models.Model, utils.ClonableMixin):
 
 class VersionedModel(VersionedModelBase):
     vid = models.AutoField(primary_key=True)
-    
+
     class Meta:
         abstract = True
 
@@ -268,19 +268,19 @@ class TrashableModel(models.Model):
     """ Users wanting a version history may also expect a trash bin
     that allows them to recover deleted content, as is e.g. the
     case in WordPress. This is that thing. """
-    
+
     _is_trash = models.BooleanField(db_column='is_trash', default=False, editable=False)
-    
+
     @property
     def is_trash(self):
         return self._is_trash
-    
+
     def get_content_bundle(self):
         if isinstance(self, VersionedModelBase):
             return self.get_revisions()
         else:
-            return [self]        
-    
+            return [self]
+
     def delete(self):
         """
         It makes no sense to trash individual revisions: either you keep a version history or you don't.
@@ -289,10 +289,10 @@ class TrashableModel(models.Model):
         for obj in self.get_content_bundle():
             obj._is_trash = True
             obj.save()
-    
-    def delete_permanently(self):    
+
+    def delete_permanently(self):
         for obj in self.get_content_bundle():
             super(TrashableModel, obj).delete()
-    
+
     class Meta:
         abstract = True
