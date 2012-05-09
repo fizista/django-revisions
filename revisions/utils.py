62ec641f9222eef3cdd61908e86ef7b86248234b
Lukas Pirl <lukas.pirl@student.hpi.uni-potsdam.de>
2012-05-09 17:58:01 +0200

    fixed AttributeError during clone w/ ManyToManyField if a model had a ManyToManyField with a custom intermediate model, there is no add() on that ManyRelatedManager

--- revisions/utils.py
+++ revisions/utils.py
@@ -7,12 +7,12 @@ except:
 
 # Since Django 1.2, a simple copy.copy(model) w/ pk = None stopped working.
 class ClonableMixin(object):
-    def clone(self):    
+    def clone(self):
         duplicate = self.__class__()
         for field in self._meta.fields:
             pk = field.primary_key
             comparator = (field.name is self.comparator_name)
-            # people expect these fields to work per-bundle, not per-revision, 
+            # people expect these fields to work per-bundle, not per-revision,
             # so we'll overwrite these values with the old ones
             auto_field = \
                 isinstance(field, CreationDateTimeField) or \
@@ -21,14 +21,17 @@ class ClonableMixin(object):
             if not (pk or auto_field or comparator):
                 value = getattr(self, field.name)
                 setattr(duplicate, field.name, value)
-        
+
         duplicate.save()
-        
+
         # ... but the trick loses all ManyToMany relations.
         for field in self._meta.many_to_many:
             source = getattr(self, field.attname)
             destination = getattr(duplicate, field.attname)
             for item in source.all():
-                destination.add(item)
+                try:
+                    destination.add(item)
+                except AttributeError:
+                    pass
 
         return duplicate
